1d9e9380d44f5875a7934ac8520be9c8d8310e4e
Aleksei <example@gmail.com>
2023-01-11 20:03:08 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// let number = 5.4;
+
+// const person = '5';
+
+// const bool = false;
+
+// let und;
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log (obj.name);
+// console.log (obj['name']);
+
+// let arr = ['plum.png', 'orange.jpeg', 6, 'apple.bmp'];
+
+// alert('Hello');
+
+// const result = confirm("Are you stil here?"); 
+// console.log(result);
+
+const answers = [];
+
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your surname?', '');
+answers[2] = prompt('What is your age?', '');
+
+document.write(answers);
